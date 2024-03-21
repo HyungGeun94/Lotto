debe6941d993e0247560233e2a0d200091025019
KimJongHoss <haberde97@gmail.com>
2024-03-21 10:00:54 +0900

    일단 덮어쓰기

--- src/main/java/hg/jh/luko6/entity/Lotto.java
+++ src/main/java/hg/jh/luko6/entity/Lotto.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
 @Entity
 @Log4j2
+@Data
 public class Lotto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
